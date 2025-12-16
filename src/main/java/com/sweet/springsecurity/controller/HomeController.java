71dd0d33188ce81bc5c93c3e6f25aa3e4bf2aad9
MissSweet <phamminhhoa3005@gmail.com>
2025-12-16 14:14:08 +0700

    example Spring Security with filterChain

---
+++ src/main/java/com/sweet/springsecurity/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.sweet.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/admin")
+    public String adminMethod(){
+        return "This is admin method";
+    }
+
+    @GetMapping("/user")
+    public String userMethod(){
+        return "This is user method";
+    }
+
+    @GetMapping("/other")
+    public String otherMethod(){
+        return "This is other method";
+    }
+}
